e988e972cc6b5c32f4e229504081ec55f7bbd2a5
Arman Salehi <arman.salehi@student.rocva.nl>
2021-02-05 12:04:53 +0100

    fontend alert,ifelse,prompt,confirm

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,5 +1,10 @@
 strNaam = prompt("Hoe heet je?");
 boolNaamCorrect = confirm("Heet je echt " + strNaam +"?"); 
-if (boolNaamCorrect = TRUE) {
-    alert("Welkom!");
+
+if(boolNaamCorrect == true) {
+    alert("Welkom, " + strNaam +"!");
+}
+
+else {
+    alert("Oei, ik weet niet hoe je heet, toch van harte welkom!");
 }
